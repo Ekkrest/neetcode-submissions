bdf0ae4a18d66083cb1a0fe879445519fc613982
Ekkrest <73702121+Ekkrest@users.noreply.github.com>
2026-05-18 14:16:00 +0800

    Add: anagram-groups - submission-1

---
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> umap;
+
+        for(string& s : strs){
+            string key = s;
+            sort(key.begin(), key.end());
+            umap[key].push_back(s);
+        }
+
+        vector<vector<string>> ans;
+        ans.reserve(umap.size());
+
+        for(auto& pair : umap){
+            ans.push_back(move(pair.second));
+        }
+
+        return ans;
+    }
+};
