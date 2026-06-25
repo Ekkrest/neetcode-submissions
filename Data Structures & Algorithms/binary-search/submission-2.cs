4dfb637279e395c7977310f26d393a02161a12bc
Ekkrest <73702121+Ekkrest@users.noreply.github.com>
2026-06-25 22:16:34 +0800

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.Length - 1;
+
+        while(left <= right){
+            int middle = left + (right - left) / 2;
+            if(nums[middle] == target){
+                return middle;
+            }
+            else if(nums[middle] > target){
+                right = middle - 1;
+            }else{
+                left = middle + 1;
+            }
+        }
+
+        return -1;
+    }
+}
