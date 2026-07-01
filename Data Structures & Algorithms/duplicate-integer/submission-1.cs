ea09007f246aa0b39e4f68c978d79efe20f299aa
Ekkrest <73702121+Ekkrest@users.noreply.github.com>
2026-07-01 13:26:46 +0800

    Add: duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        Dictionary<int, int> map = new Dictionary<int, int>();
+
+        for(int i=0; i<nums.Length; i++){
+            if(!map.ContainsKey(nums[i])){
+                map[nums[i]] = 1;
+            }else{
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
